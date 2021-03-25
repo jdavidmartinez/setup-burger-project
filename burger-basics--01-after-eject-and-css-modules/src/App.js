305be9488e0f05176925c71d84ea07138051361f
DaviD D <jdavid.martinez@gmail.com>
2021-03-24 23:34:59 -0500

    Infra: making BurgerBuilder component [125]

--- burger-basics--01-after-eject-and-css-modules/src/App.js
+++ burger-basics--01-after-eject-and-css-modules/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import Layout from '../src/components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div >
         <Layout>
-          <h>Test</h>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
