6d3cbc767e87b9c0ef82921bf7d20e2a871fa1cd
DaviD D <jdavid.martinez@gmail.com>
2021-03-27 19:40:18 -0500

    Video130: validating if tehre is ingredients

--- burger-basics--01-after-eject-and-css-modules/src/components/Layout/Burger/Burger.js
+++ burger-basics--01-after-eject-and-css-modules/src/components/Layout/Burger/Burger.js
@@ -4,12 +4,18 @@ import clasess from './Burger.css';
 
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
-    const transformedIngredients= Object.keys(props.ingredients)
+    let transformedIngredients= Object.keys(props.ingredients)
         .map(igKey => {
             return[...Array(props.ingredients[igKey])].map((_, i) =>{
                 return <BurgerIngredient key={igKey+i} type={igKey} />;
             })
-        });
+        })
+        .reduce((arr, el)=>{
+            return arr.concat(el)
+        }, []);
+        if(transformedIngredients.length === 0){
+            transformedIngredients = <p>Please start adding ingredient!</p>
+        }
     return (
         <div className={clasess.Burger}>
             <BurgerIngredient type="bread-top"/>
