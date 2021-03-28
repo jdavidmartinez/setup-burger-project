e04ce4f7a82c90448cfe470091a575fa30601a61
DaviD D <jdavid.martinez@gmail.com>
2021-03-28 14:44:19 -0500

    138. Showing & Hiding the Modal (with Animation)

--- burger-basics--01-after-eject-and-css-modules/src/components/UI/Modal/Modal.js
+++ burger-basics--01-after-eject-and-css-modules/src/components/UI/Modal/Modal.js
@@ -3,7 +3,12 @@ import React from 'react';
 import clasess from './Modal.css'
 
 const modal = (props) =>(
-    <div className={clasess.Modal}>
+    <div 
+    className={clasess.Modal}
+    style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1': '0'
+    }}>
         {props.children}
     </div>
 );
