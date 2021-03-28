1aab6a2b0abc05a40a66c7abb31758856300e2ad
DaviD D <jdavid.martinez@gmail.com>
2021-03-27 19:16:24 -0500

    Video129: making dinamic ingredients using state

--- burger-basics--01-after-eject-and-css-modules/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-basics--01-after-eject-and-css-modules/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,10 +6,23 @@ import Burger from '../../components/Layout/Burger/Burger'
 
 
 class BurgerBuilder extends Component {
+    //constructor(props){
+    //    super(props);
+    //     this.state{...};
+
+    // }
+    state = {
+        ingredients:{
+            salad: 3,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
     render () {
         return (
             <Aux>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Control</div>
             </Aux>
         );
